f7c6d58630f3fed1a92df5668ed98c3d951bf852
Eunice Elizabeth Dhivya <eunicedhivya@thefederal.in>
2020-01-23 13:58:03 +0530

    add medals to modal content with layout css

--- js/main.js
+++ js/main.js
@@ -12,12 +12,14 @@ $(".defence-force").on("click", function(){
 
     $("header").css("display", "none")
     $(".defence-force").css("display", "none")
+    $(".medals").css("display", "block")
     $(this).css("display", "flex")
     $(this).css("width", "100vw")
     $(this).css("height", "100vh")
     $(this).addClass("active")
     var html = "<button class=\"closebtn\">X</button>"
     $(this).append(html)
+
     
 
 })
@@ -25,6 +27,7 @@ $(".defence-force").on("click", function(){
 $(document).on('click', '.closebtn', function(){ 
     // Your Code
     $("header").css("display", "flex")
+    $(".medals").css("display", "none")
     $(".defence-force").removeAttr("style")
     $(".defence-force").removeClass("active")
 });
